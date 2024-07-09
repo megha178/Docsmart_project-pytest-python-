0add21b156cb4d6d444f160507fe57a5c98dbd65
megha178 <36306749+megha178@users.noreply.github.com>
2024-07-09 16:55:12 +0530

    2nd commit

--- tests/DocSmart_login/test_docsmartlogin.py
+++ tests/DocSmart_login/test_docsmartlogin.py
@@ -14,21 +14,32 @@ class TestDocSmart:
     #   @pytest.mark.positive
 
     @pytest.mark.qa
-    @pytest.mark.usefixtures("setup")
-    def test_invalid_login(self, setup):
-        driver = setup
+    @pytest.mark.usefixtures("driver_init")
+    @pytest.mark.usefixtures("login")
+
+   # @pytest.mark.usefixtures("setup")
+   # @pytest.mark.parametrize("browser_name", ["chrome", "firefox"])
+    def test_invalid_login(self,login):
+       # driver = driver_init
+        driver = login
         loginPage = LoginPage(driver)
-        loginPage.login_patient(user="2333222222", pwd=self.password)
+        loginPage.login_patient(user="2333222222", pwd="Admin@123")
         assert loginPage.get_error_msg().is_displayed()
         allure.attach(driver.get_screenshot_as_png(), name="invalid_login", attachment_type=AttachmentType.PNG)
 
     @pytest.mark.smoke
-    @pytest.mark.usefixtures("setup")
-    def test_valid_login(self, setup):
-        driver = setup
+   # @pytest.mark.usefixtures("setup")
+    @pytest.mark.usefixtures("driver_init")
+    @pytest.mark.usefixtures("login")
+
+ #   @pytest.mark.parametrize("browser_name", ["chrome"])
+    def test_valid_login(self,login):
+       # driver = driver_init
+        driver = login
         loginPage = LoginPage(driver)
-        loginPage.login_patient(user=self.username, pwd=self.password)
+        loginPage.login_patient(user="7021656297", pwd="Admin@123")
         time.sleep(5)
         print(self.username)
         print(self.password)
         assert "https://app.docsmart.in/patient/dashboard" in driver.current_url
+        driver.save_screenshot('dashboard.png')
